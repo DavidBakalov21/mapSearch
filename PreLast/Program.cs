3b8c202e3f1fc8e25fbf3e6c19a1c52873bb76d1
DavidBakalov21 <davidbakalov33@gmail.com>
2023-05-29 16:41:39 +0300

    next

--- PreLast/Program.cs
+++ PreLast/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.Globalization;
 using PreLast;
 
+
 /*
 using System.Diagnostics;
 
@@ -53,18 +54,37 @@ Console.WriteLine($"Elapsed time: {sw.Elapsed}");
 */
 
 
-var words = File.ReadAllLines("C:\\Users\\Давід\\RiderProjects\\PreLast\\PreLast\\ukraine_poi.csv");
+var words = File.ReadAllLines("C:\\Users\\Давід\\RiderProjects\\PreLast\\PreLast\\test.csv");
 var latList = new List<double>();
 var longList = new List<double>();
+var Points = new List<Point>();
 for (int i = 0; i < words.Length; i++)
 {
+    
     var split = words[i].Split(";");
-    var latitude = split[0];
-    var longitude = split[1];
+   
+   
+    var latitude = split[0].Replace(",", ".");;
+    var longitude = split[1].Replace(",", ".");;
     if (latitude != "" && longitude != "" && latitude != null && longitude != null)
     {
-        latList.Add(double.Parse(latitude));
-        longList.Add(double.Parse(longitude));
+        var t = split.Length;
+        var name = "";
+        while (split[t-1]=="")
+        {
+            if (split[t-1]!="")
+            {
+                break;
+            }
+
+            t -= 1;
+        }
+       
+            name = split[t-1];
+      
+        latList.Add(double.Parse(latitude, CultureInfo.InvariantCulture));
+        longList.Add(double.Parse(longitude, CultureInfo.InvariantCulture));
+        Points.Add(new Point(name, double.Parse(longitude, CultureInfo.InvariantCulture),double.Parse(latitude, CultureInfo.InvariantCulture)));
     }
 }
 
@@ -78,3 +98,65 @@ Console.WriteLine(bottom_leftPoint.longitude);
     
     
     // throw pointlist and radius to func 
+
+foreach (var VARIABLE in Points)
+{
+    var output = VARIABLE.name+" " + VARIABLE.latitude+" "+ VARIABLE.longitude;
+    Console.WriteLine(output);
+}
+KdTree buildTree(List<Point> pointList)
+{
+    KdTree kdTree = new KdTree();
+    kdTree.BuildTree(pointList);
+    return kdTree;
+}
+
+var a = buildTree(Points);
+var b= buildTree(Points);
+
+
+class Node
+{
+    public Point value { get; set; }
+    public Node left { get; set; }
+    public Node Right { get; set; }
+
+    public Node(Point Value)
+    {
+        value = Value;
+    }
+}
+
+class KdTree
+{
+    public Node Root { get; set; }
+
+    public KdTree()
+    {
+        Root = null;
+    }
+
+    public void BuildTree(List<Point> pointList)
+    {
+        Root = BuildTreeRecursive(pointList, 0);
+    }
+
+    private Node BuildTreeRecursive(List<Point> pointList, int depth)
+    {
+        if (pointList.Count == 0)
+        {
+            return null;
+        }
+
+        int axis = depth % 2; // Alternate between x-axis (longitude) and y-axis (latitude)
+        pointList.Sort((a, b) => axis == 0 ? a.longitude.CompareTo(b.longitude) : a.latitude.CompareTo(b.latitude));
+        int medianIndex = pointList.Count / 2;
+        Point medianPoint = pointList[medianIndex];
+
+        Node node = new Node(medianPoint);
+        node.left = BuildTreeRecursive(pointList.GetRange(0, medianIndex), depth + 1);
+        node.Right = BuildTreeRecursive(pointList.GetRange(medianIndex + 1, pointList.Count - medianIndex - 1), depth + 1);
+
+        return node;
+    }
+}
