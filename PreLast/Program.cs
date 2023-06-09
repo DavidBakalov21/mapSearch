deb6a0cba1dfa3c800b6136224c04efee0f78c76
DavidBakalov21 <davidbakalov33@gmail.com>
2023-06-09 10:17:59 +0300

    fvdsfs

--- PreLast/Program.cs
+++ PreLast/Program.cs
@@ -3,59 +3,24 @@ using System.Globalization;
 using PreLast;
 
 
-/*
+
 using System.Diagnostics;
+Rect ToRect(double Radius, double latitude, double longitude)
+{
+    var resRect = new Rect(new Point("name", latitude - Radius, longitude - Radius),
+        new Point("name", latitude + Radius, longitude + Radius));
+    return resRect;
+}
+
 
 var sw = new Stopwatch();
 sw.Start();
-var  Words= File.ReadAllLines("C:\\Users\\Давід\\RiderProjects\\PreLast\\PreLast\\ukraine_poi.csv");
-//Console.WriteLine(CalculateDistance( 50.44075, 30.55263,  48.89283,  22.40971));
+
+var words = File.ReadAllLines("C:\\Users\\Давід\\RiderProjects\\PreLast\\PreLast\\ukraine_poi.csv");
 var latitude1 = Console.ReadLine();
 var longitude1 = Console.ReadLine();
 var radius = Console.ReadLine();
-var Places = new List<string>();
-var t = 4;
-for (int i = 0; i < Words.Length; i++)
-{
-    var split = Words[i].Split(";");
-    var latitude = split[0].Replace(",", ".");
-    var longitude = split[1].Replace(",", ".");
-    if (latitude!="" && longitude!="" && latitude!=null && longitude!=null)
-    {
-        var distance=CalculateDistance( double.Parse(latitude1, CultureInfo.InvariantCulture), double.Parse(longitude1, CultureInfo.InvariantCulture),  double.Parse(latitude, CultureInfo.InvariantCulture),  double.Parse(longitude, CultureInfo.InvariantCulture));
-
-        if (distance/1000<=double.Parse(radius, CultureInfo.InvariantCulture))
-        {
-          
-            Places.Add(Words[i]+"--distance--"+distance/1000);
-        }  
-    }
-}
-
-foreach (var VARIABLE in Places)
-{
-    Console.WriteLine(VARIABLE);
-}
-double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
-{
-
-    double rLat =(lat2 - lat1)* (Math.PI / 180);
-    double rLon = (lon2 - lon1)* (Math.PI / 180);
-
-    double a = Math.Sin(rLat/2) * Math.Sin(rLat/2) +
-        Math.Cos(lat1 * Math.PI/180) * Math.Cos(lat2 * Math.PI/180) *
-        Math.Sin(rLon/2) * Math.Sin(rLon/2);
-    double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-    double distance =6371e3 * c;
-    return distance;
-}
-
-sw.Stop();
-Console.WriteLine($"Elapsed time: {sw.Elapsed}");
-*/
-
-
-var words = File.ReadAllLines("C:\\Users\\Давід\\RiderProjects\\PreLast\\PreLast\\ukraine_poi.csv");
+var RadiusRect = ToRect(double.Parse(radius), double.Parse(latitude1), double.Parse(longitude1));
 var latList = new List<double>();
 var longList = new List<double>();
 var Points = new List<Point>();
@@ -65,8 +30,8 @@ for (int i = 0; i < words.Length; i++)
     var split = words[i].Split(";");
    
    
-    var latitude = split[0].Replace(",", ".");;
-    var longitude = split[1].Replace(",", ".");;
+    var latitude = split[0];
+    var longitude = split[1];
     if (latitude != "" && longitude != "" && latitude != null && longitude != null)
     {
         var t = split.Length;
@@ -83,54 +48,48 @@ for (int i = 0; i < words.Length; i++)
        
             name = split[t-1];
       
-        latList.Add(double.Parse(latitude, CultureInfo.InvariantCulture));
-        longList.Add(double.Parse(longitude, CultureInfo.InvariantCulture));
-        Points.Add(new Point(name, double.Parse(latitude, CultureInfo.InvariantCulture),double.Parse(longitude, CultureInfo.InvariantCulture)));
+        latList.Add(double.Parse(latitude));
+        longList.Add(double.Parse(longitude));
+        Points.Add(new Point(name, double.Parse(latitude),double.Parse(longitude)));
     }
 }
 
 var bottom_leftPoint= new Point("bottom_left", latList.Min(),longList.Min() );
 var top_rightPoint= new Point("top_right", latList.Max(),longList.Max() );
-//create a point class(lat, long , type)-Done
-    // craete  rect (class point) (min lat min long; max lat max lat)
-    //class tree with root(class) (in all classes rect heir)
-    
-    
-    // throw pointlist and radius to func 
-
-foreach (var VARIABLE in Points)
-{
-    var output = VARIABLE.name+" " + VARIABLE.latitude+" "+ VARIABLE.longitude;
-   // Console.WriteLine(output);
-}
-KdTree buildTree(List<Point> pointList, rect StartRect)
+KdTree buildTree(List<Point> pointList, Rect StartRect)
 {
     KdTree kdTree = new KdTree();
     kdTree.BuildTree(pointList, StartRect);
     return kdTree;
 }
 
-var StartRect = new rect(bottom_leftPoint, top_rightPoint);
+var StartRect = new Rect(bottom_leftPoint, top_rightPoint);
 var a = buildTree(Points,StartRect);
-var b= buildTree(Points,StartRect);
+var FinList = a.Traverse(RadiusRect, new Point("name", double.Parse(latitude1), double.Parse(longitude1)), double.Parse(radius));
 
-
+foreach (var VARIABLE in FinList)
+{
+    var output = VARIABLE.name+" " + VARIABLE.latitude+" "+ VARIABLE.longitude;
+     Console.WriteLine(output);
+}
+sw.Stop();
+Console.WriteLine($"Elapsed time: {sw.Elapsed}");
 class Node
 {
     public List<Point> value { get; set; }
    
-    public rect rectvalue { get; set; }
+    public Rect rectvalue { get; set; }
     public Node left { get; set; }
-    public Node Right { get; set; }
+    public Node right { get; set; }
 
-    public Node(List<Point> Value, rect RectVal)
+    public Node( Rect RectVal)
     {
-        value = Value;
         rectvalue = RectVal;
-
+        value = new List<Point>();
+        left = null;
+        right = null;
     }
 }
-
 class KdTree
 {
     public Node Root { get; set; }
@@ -140,72 +99,94 @@ class KdTree
         Root = null;
     }
 
-    public void BuildTree(List<Point> pointList, rect Strtrect)
+    public void BuildTree(List<Point> pointList, Rect strtRect)
     {
-        Root = BuildTreeRecursive(pointList, 0, Strtrect);
+        Root = BuildTreeRecursive(pointList, 0, strtRect);
     }
 
-    private Node BuildTreeRecursive(List<Point> pointList, int depth, rect startRect)
+    private Node BuildTreeRecursive(List<Point> pointList, int depth, Rect startRect)
     {
+        if (pointList.Count == 0)
+            return null;
 
-
-        if (pointList.Count <= 10)
+        if (pointList.Count <= 2)
         {
-            
-            return new Node(pointList,startRect);
+            Node leafNode = new Node(startRect);
+            leafNode.value.AddRange(pointList);
+            return leafNode;
         }
 
-
-        int axis = depth % 2; 
+        int axis = depth % 2;
         pointList.Sort((a, b) => axis == 0 ? a.longitude.CompareTo(b.longitude) : a.latitude.CompareTo(b.latitude));
 
         int medianIndex = pointList.Count / 2;
         Point medianPoint = pointList[medianIndex];
 
-        rect newRectL;
-        rect newRectR;
+        Rect newRectL, newRectR;
         if (axis == 0)
         {
-            newRectL = new rect(startRect.BottomLeft, new Point("name", medianPoint.latitude, startRect.TopRight.longitude ));
-            newRectR = new rect(new Point("name", medianPoint.latitude, startRect.BottomLeft.longitude ), startRect.TopRight);
-            
+            newRectL = new Rect(startRect.BottomLeft, new Point("name", medianPoint.latitude, startRect.TopRight.longitude));
+            newRectR = new Rect(new Point("name", medianPoint.latitude, startRect.BottomLeft.longitude), startRect.TopRight);
         }
         else
         {
-            newRectL = new rect(new Point("name", startRect.BottomLeft.latitude,  medianPoint.longitude), startRect.TopRight);
-            newRectR = new rect(startRect.BottomLeft, new Point("name", startRect.TopRight.latitude, medianPoint.longitude ));
+            newRectL = new Rect(new Point("name", startRect.BottomLeft.latitude, medianPoint.longitude), startRect.TopRight);
+            newRectR = new Rect(startRect.BottomLeft, new Point("name", startRect.TopRight.latitude, medianPoint.longitude));
         }
 
-        Node node = new Node(null, startRect);
+        Node node = new Node(startRect);
         node.left = BuildTreeRecursive(pointList.GetRange(0, medianIndex), depth + 1, newRectL);
-        node.Right = BuildTreeRecursive(pointList.GetRange(medianIndex + 1, pointList.Count - medianIndex - 1), depth + 1, newRectR);
+        node.right = BuildTreeRecursive(pointList.GetRange(medianIndex + 1, pointList.Count - medianIndex - 1), depth + 1, newRectR);
 
         return node;
     }
 
-    public List<Point> Traverse(rect Rect, Point point, Node node)
+    public List<Point> Traverse(Rect rect, Point point, double radius)
     {
-        
-      var  traverseLeft= Traverse(node.rectvalue, point,node.left);
-      var  traverseRight= Traverse(node.rectvalue, point,node.Right);
-      
-      if (traverseLeft == null)
-      {
-          var list = node.value;
-          foreach (var VARIABLE in list)
-          {
-              
-          }
+        if (Root == null || !rect.Intersect(Root.rectvalue))
+        {
+            return new List<Point>();
+        }
 
-      }
-      if (traverseRight == null)
-      {
-          var list = node.value;
-          foreach (var VARIABLE in list)
-          {
-              
-          }
-      }
-        return null;
+        List<Point> resultList = new List<Point>();
+
+        TraverseRecursive(rect, point, Root, radius, resultList);
+
+        return resultList;
+    }
+
+    private void TraverseRecursive(Rect rect, Point point, Node node, double radius, List<Point> resultList)
+    {
+        if (node == null || !rect.Intersect(node.rectvalue))
+        {
+            return;
+        }
+
+        if (node.left == null && node.right == null)
+        {
+            foreach (var p in node.value)
+            {
+                double distance = CalculateDistance(point.latitude, point.longitude, p.latitude, p.longitude);
+                if (distance / 1000 <= radius)
+                {
+                    resultList.Add(p);
+                }
+            }
+        }
+        else
+        {
+            TraverseRecursive(rect, point, node.left, radius, resultList);
+            TraverseRecursive(rect, point, node.right, radius, resultList);
+        }
+    }
+
+    private double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
+    {
+        double rLat = (lat2 - lat1) * (Math.PI / 180);
+        double rLon = (lon2 - lon1) * (Math.PI / 180);
+        double a = Math.Sin(rLat / 2) * Math.Sin(rLat / 2) + Math.Cos(lat1 * Math.PI / 180) * Math.Cos(lat2 * Math.PI / 180) * Math.Sin(rLon / 2) * Math.Sin(rLon / 2);
+        double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        double distance = 6371e3 * c;
+        return distance;
     }
 }
--- PreLast/rect.cs
+++ PreLast/rect.cs
@@ -2,14 +2,25 @@
 
 
 
-public class rect
+public class Rect
 {
     public Point BottomLeft;
     public Point TopRight;
-    public rect(Point bottomLeft, Point topRight)
+    public Rect(Point bottomLeft, Point topRight)
     {
 
         this.BottomLeft = bottomLeft;
         this.TopRight = topRight;
     }
+
+    public bool Intersect(Rect Another)
+    {
+        var Intersection = false;
+        if ((this.TopRight.latitude>Another.BottomLeft.latitude || Another.TopRight.latitude>this.BottomLeft.latitude) && (this.TopRight.longitude>Another.BottomLeft.longitude || Another.TopRight.longitude>this.BottomLeft.longitude))
+        {
+            Intersection = true;
+        }
+        return Intersection;
+    }
+
 }
